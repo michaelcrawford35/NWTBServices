0e0ce442a05c915099b63677a33b00f368a813bf
Michael Burnes <me@mtburnes.dev>
2021-03-18 00:34:24 -0500

    Added homepage text

--- src/main/java/com/reactspring/reactspringtutorial/app/src/NWTBHome.js
+++ src/main/java/com/reactspring/reactspringtutorial/app/src/NWTBHome.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './NWTBApp.css'
 import NWTBReactNavbar from "./NWTBReactNavbar";
 
+//TODO: Add flavor to own component
 class NWTBHome extends Component {
     render() {
         return (
             <div>
                 <NWTBReactNavbar/>
+                <h1>Welcome!</h1>
+                <h5>NWTBS Alpha v1.0</h5>
             </div>
         );
     }
